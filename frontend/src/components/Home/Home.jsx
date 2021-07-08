566c355cd846f3ad6a9f31835f958c333bcdb2ec
Jean Grijp <grijpjean@gmail.com>
2021-07-07 23:00:39 -0300

    .

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import styles from './style.module.scss';
-import logoDescktop from '../../imagens/desktop_logo.svg'
+import logoDesktop from '../../imagens/desktop_logo.svg'
 
 
 export default function Home () {
@@ -10,27 +10,35 @@ export default function Home () {
       <div className={styles.containerSon}>
 
         <div className={styles.logoImg}>
-          <h2 className="doIt">do it!</h2>
-          <img src={logoDescktop} alt="logo" srcset="" />
+
+          <div className={styles.logo}>
+            <h2 className="doIt">do it!</h2>
+          </div>
+
+          <div className={styles.containerLogin}>
+
+            <div className={styles.emailArea}>
+              <label htmlFor="Email">Email</label>
+              <input type="email" name="email"/>
+            </div>
+
+            <div className={styles.passwordArea}>
+              <label htmlFor="Password">Senha</label>
+              <input type="password" name="password"/>
+            </div>
+
+            <div className={styles.inputSumbit}>
+              <input type="submit" value="Continuar" />
+            </div>
+
+          </div>
+
         </div>
 
-        <div className={styles.containerLogin}>
-
-          <div className={styles.emailArea}>
-            <label htmlFor="Email">Email</label>
-            <input type="email" name="email"/>
-          </div>
-
-          <div className={styles.passwordArea}>
-            <label htmlFor="Password">Senha</label>
-            <input type="password" name="password"/>
-          </div>
-
-          <div className={styles.inputSumbit}>
-            <input type="submit" value="Continuar" />
-          </div>
-
+        <div className={styles.img}>
+          <img src={logoDesktop} alt="logo" srcset="" />
         </div>
+
 
       </div>
 
